f9f35ffd7f70839882ddd9f46360cd8fa71c6e77
Grigore Stefan <g_stefan@yahoo.com>
2020-11-02 02:33:03 +0200

    update

--- build.config.cmd
+++ build.config.cmd
@@ -3,4 +3,6 @@ rem Public domain
 rem http://unlicense.org/
 rem Created by Grigore Stefan <g_stefan@yahoo.com>
 
+set PRODUCT_NAME=installer-phpmyadmin
 set PRODUCT_VERSION=5.0.4
+set PRODUCT_BASE=phpmyadmin
