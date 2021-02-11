0220e5af1fad65ea0023c681a04f0158c57aa11a
Grigore Stefan <g_stefan@yahoo.com>
2021-02-12 01:41:38 +0200

    update

--- util/phpmyadmin.config.cmd
+++ util/phpmyadmin.config.cmd
@@ -6,7 +6,7 @@ rem Created by Grigore Stefan <g_stefan@yahoo.com>
 setlocal ENABLEEXTENSIONS
 
 for /f "tokens=2*" %%a in ('reg.exe query "HKLM\Software\phpMyAdmin" /v InstallPath /reg:64 2^>^&1^|find "REG_"') do set PHPMYADMIN_INSTALL_DIR=%%b
-for /f "tokens=2*" %%a in ('reg.exe query "HKLM\Software\PHP" /v InstallPath /reg:64 2^>^&1^|find "REG_"') do set PHP_DIR=%%b
+for /f "tokens=2*" %%a in ('reg.exe query "HKLM\Software\Hypertext-Preprocessor" /v InstallPath /reg:64 2^>^&1^|find "REG_"') do set PHP_DIR=%%b
 
 pushd "%ProgramData%\HTTPD\www\phpmyadmin"
 "%PHP_DIR%\php.exe" "%PHPMYADMIN_INSTALL_DIR%\installer.config.inc.php"
