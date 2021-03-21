eca6ef466611ee362af240cd4faa0410afd07044
Grigore Stefan <g_stefan@yahoo.com>
2021-03-21 23:58:12 +0200

    update

--- build.config.cmd
+++ build.config.cmd
@@ -4,5 +4,5 @@ rem http://unlicense.org/
 rem Created by Grigore Stefan <g_stefan@yahoo.com>
 
 set PRODUCT_NAME=installer-phpmyadmin
-set PRODUCT_VERSION=5.0.4
+set PRODUCT_VERSION=5.1.0
 set PRODUCT_BASE=phpmyadmin
