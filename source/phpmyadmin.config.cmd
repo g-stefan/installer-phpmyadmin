9b01d826d2e32ab024239ad16973c8966af06951
Grigore Stefan <g_stefan@yahoo.com>
2024-01-02 22:50:34 +0200

    Update

--- source/phpmyadmin.config.cmd
+++ source/phpmyadmin.config.cmd
@@ -1,7 +1,7 @@
 @echo off
 rem Created by Grigore Stefan <g_stefan@yahoo.com>
 rem Public domain (Unlicense) <http://unlicense.org>
-rem SPDX-FileCopyrightText: 2020-2023 Grigore Stefan <g_stefan@yahoo.com>
+rem SPDX-FileCopyrightText: 2020-2024 Grigore Stefan <g_stefan@yahoo.com>
 rem SPDX-License-Identifier: Unlicense
 
 setlocal ENABLEEXTENSIONS
